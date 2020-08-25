b889f8220c5e070d64de15b7ce70241b83d5daf8
cristiaan3003 <cristiaan3003@gmail.com>
2020-08-25 16:04:07 -0300

    change init.rb name version

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :proforma do
-  name 'Proforma plugin'
+  name 'Proforma 2.0 plugin'
   author 'Folder IT'
   description 'Proforma stuff Nacho Juan FTW'
-  version '0.1.0'
+  version '0.2.0'
   settings :default => {:empty => true}, :partial => 'settings/proforma_settings'
   
   project_module :proforma do
@@ -11,5 +11,5 @@ Redmine::Plugin.register :proforma do
     permission :block_proforma, :proforma => :block_proforma
   end
 
-  menu :project_menu, :proforma, {:controller => 'proforma', :action => 'index'}, :caption => 'Proforma', :after => :activity, :param => :project_id
+  menu :project_menu, :proforma, {:controller => 'proforma', :action => 'index'}, :caption => 'Proforma2', :after => :activity, :param => :project_id
 end
